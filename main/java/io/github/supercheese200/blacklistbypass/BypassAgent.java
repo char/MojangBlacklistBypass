e3f53426e70e8ed835fc0dec20cfb244ddee3381
supercheese200 <supercheesedoublehundred@gmail.com>
2016-05-15 12:08:36 +0100

    Initial commit

---
+++ main/java/io/github/supercheese200/blacklistbypass/BypassAgent.java
@@ -0,0 +1,57 @@
+package io.github.supercheese200.blacklistbypass;
+
+import javassist.ByteArrayClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+public class BypassAgent implements ClassFileTransformer {
+    /**
+     * Pre-main - Called when the agent is invoked at the startup of a JVM.
+     * @param agentArgs The launch arguments of the Java agent
+     * @param instrumentation An Instrumentation instance, allowing us to add transformers.
+     */
+    public static void premain(String agentArgs, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new BypassAgent());
+    }
+
+
+    public static void agentmain(String args, Instrumentation instrumentation) {
+        throw new RuntimeException("Post-startup agent not supported!");
+    }
+
+
+    /**
+     * Transformation method - implemented by a ClassFileTransformer in order to change classes' bytecode as they are loaded.
+     * @param classLoader The ClassLoader of the class being transformed.
+     * @param className The class name passed from the JVM.
+     * @param classBeingTransformed The Class object of the class being transformed.
+     * @param protectionDomain The protection domain of the class being transformed.
+     * @param bytes The bytecode of the class being transformed.
+     * @return The redefined array of bytes.
+     * @throws IllegalClassFormatException
+     */
+    @Override
+    public byte[] transform(ClassLoader classLoader, String className, Class<?> classBeingTransformed, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
+        try {
+            if (classBeingTransformed.getName().equals("io.netty.bootstrap.Bootstrap")) {
+                ClassPool pool = ClassPool.getDefault();
+                pool.appendClassPath(new ByteArrayClassPath(classBeingTransformed.getName(), bytes));
+
+                CtClass ctClass = pool.get(classBeingTransformed.getName());
+                CtMethod method = ctClass.getMethod("isBlockedServerHostName", "(Ljava/lang/String;)Z");
+                method.setBody("{ return false; }");
+
+                return ctClass.toBytecode();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
